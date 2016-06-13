7c485de3859f5fa597417eea8f043fd3b22c223e
Sergey Balog <bal323@mail.ru>
2016-06-13 16:57:48 +0300

    Rename project folders

--- src/LHR.MVC.CUST/Properties/AssemblyInfo.cs
+++ src/LHR.MVC.CUST/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LHR.MVC.CUST")]
+[assembly: AssemblyTitle("Lhr.Mvc.Cust")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LHR.MVC.CUST")]
+[assembly: AssemblyProduct("Lhr.Mvc.Cust")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
